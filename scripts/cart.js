3b32b5f9fe4cc7d0b6e867309c7834571a5d5311
Jason Chang <chihsenc@andrew.cmu.edu>
2017-10-04 11:12:10 -0400

    finish cart page

--- scripts/cart.js
+++ scripts/cart.js
@@ -6,15 +6,42 @@ for(var i = 0;i < removeButtons.length; i++) {
   removeButtons[i].addEventListener('click', removeItem, false);
 }
 
-function removeItem(){
+function removeItem() {
   var parent = document.getElementById(this.id.substring(0,1));
-  console.log(parent);
+  changeCartNum(-parseInt(document.getElementById(parent.id + "-quantity").innerHTML));
+  subtractTotal(getSubtotal(parent.id));
   parent.remove();
-  changeCartNum(-parseInt(this.id.substring(2, 3)));
+  checkEmpty();
 }
 
 function changeCartNum(increment) {
   var cartNum = document.getElementById('items-num');
   var newCartNum = parseInt(cartNum.innerHTML, 10) + increment;
   (newCartNum > 9) ? cartNum.innerHTML = newCartNum : cartNum.innerHTML = "0" + newCartNum.toString();
+}
+
+function getSubtotal(item) {
+  return parseFloat(document.getElementById(item + "-subtotal").innerHTML.substring(1));
+}
+
+function subtractTotal(dec) {
+  var totalDiv = document.getElementById("total-price").innerHTML;
+  var currPrice = parseFloat(totalDiv.substring(1));
+  var newTotal = (currPrice - dec).toFixed(2);
+  document.getElementById("total-price").innerHTML = "$" + newTotal.toString();
+}
+
+function checkEmpty() {
+  var totalDiv = document.getElementById("total-price").innerHTML;
+  var currTotal = parseFloat(totalDiv.substring(1));
+  if (currTotal === 0.00) {
+    document.getElementById("cart-table").remove();
+    document.getElementById("total-checkout").remove();
+    var div = document.getElementById("page-title");
+    var noItemsMsg = document.createElement("h3");
+    noItemsMsg.innerHTML = "No Items.";
+    noItemsMsg.style.fontWeight = "300";
+    noItemsMsg.style.fontSize = "30px";
+    div.appendChild(noItemsMsg);
+  }
 }
